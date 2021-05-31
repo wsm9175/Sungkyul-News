e78fa5894c8857c78b0fdbddc365f785afe899d9
Park-seongil98 <lewis1998@naver.com>
2021-05-31 21:23:13 +0900

    nodejs module download

---
+++ Server/app.js
@@ -0,0 +1,48 @@
+const express = require("express");
+const morgan = require("morgan");
+const cookieParser = require('cookie-parser');
+const session  = require('express-session');
+const dotenv = require('dotenv');
+const path = require('path');
+const nunjucks = require('nunjucks');
+const multer = require('multer');
+
+dotenv.config();
+const app = express(); //서버 실행
+app.use(session({ secret: 'somevalue' }));
+app.set('port', process.env.PORT || 3000);
+app.use(morgan('dev')); //서버로 들어온 요청과 응답 기록
+
+
+app.use(
+      express.json(),
+      express.static('/',path.join(__dirname, 'public')),//현재 경로로 요청
+      express.json(),
+      express.urleancoded({extended:false}),
+      cookieParser(precess.COOKIE_SECRET),
+);
+
+app.use(session({
+      resave:false,
+      saveUninitialized: false,
+      secret: process.env.COOKIE_SECRET,
+      cookie:{
+            httpOnly:true,
+            secure:false,
+      },
+      name:'sesion-cookie'
+}));
+app.listen(app.get('port'),()=>{
+      console.log(app.get('port'),'번포트에서 대기 중');
+})
+
+app.get('/', (req, res, next) => {
+      console.log('안드로이드 연결 완료');
+      next();
+}, (req, res) => {
+      throw new Error('에러는 에러 처리 미들웨어로 갑니다.')
+});
+app.use((err, req, res, next) => {
+      console.error(err);
+      res.status(500).send(err.message);
+});
