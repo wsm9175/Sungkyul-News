ba7a2ecdce73a15a7f808fb5d0c2fe797d275386
Park-seongil98 <lewis1998@naver.com>
2021-06-05 01:20:14 +0900

    signup&login

--- Server/routes/users.js
+++ Server/routes/users.js
@@ -19,4 +19,37 @@ router.post('/',(req,res)=>{
       console.log("create ID");
 })
 
+
+router.post('/login',async(req,res)=>{
+
+      var approve ={'user_id':'OK','user_password':'OK'};
+
+
+    var paramId = req.body.id;
+    var paramPassword = req.body.password;
+    console.log('id : '+paramId+'  pw : '+paramPassword);
+
+    //아이디 일치여부 flag json 데이터입니다.
+//     if(paramId == 'lewis1998') approve.approve_id = 'OK';
+//     if(paramPassword == 'park6766387') approve.approve_pw = 'OK';
+
+//     res.send(approve);
+      
+
+      var arr = await User.findAll({
+            attributes:['user_id', 'user_password','user_email','user_name'],
+            where:{
+                  user_id : paramId,
+                  user_password : paramPassword,
+            },
+            raw:true,
+      
+      })
+      console.log("1");
+      console.log(arr);
+      var result={'user_id':arr[0].user_id, 'user_name':arr[0].user_name, 'user_password':arr[0].user_password, 'user_email':arr[0].user_email};
+      console.log(result);
+       res.send(result);
+})
+
 module.exports=router;
