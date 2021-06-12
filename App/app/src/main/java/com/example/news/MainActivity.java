573d9625241c0eabc131d47f8d52eec271cc2e56
wsm9175 <76678911+wsm9175@users.noreply.github.com>
2021-06-13 00:35:08 +0900

    error correction

--- App/app/src/main/java/com/example/news/MainActivity.java
+++ App/app/src/main/java/com/example/news/MainActivity.java
@@ -70,6 +70,7 @@ public class MainActivity extends AppCompatActivity {
     private void refreshListView(){
         board_list.clear();
         getNews();
+        spinner.setSelection(0);
     }
 
     @Override
