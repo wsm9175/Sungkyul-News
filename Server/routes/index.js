f8c76ff33c7d25f236a7119f0d57e0af7d920fb2
Park-seongil98 <lewis1998@naver.com>
2021-06-13 15:06:00 +0900

    email, register

--- Server/routes/index.js
+++ Server/routes/index.js
@@ -4,5 +4,6 @@ const router = express.Router();
 router.use('/users',require("./users"));
 router.use('/boards',require("./boards"));
 router.use('/comments',require("./comments"));
+router.use('/mail',require("./auth"));
 
 module.exports=router;
